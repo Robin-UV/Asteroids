2b19520c52d7d69f9085356620d0b66552bf51e7
Robie Urbina <robieurbina@robies-mbp.mynetworksettings.com>
2025-07-06 05:43:09 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.locals import *
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    numpass, numfail = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    while (True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
